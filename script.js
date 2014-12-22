ba9a5625844571d254d714af28ddf6face14176e
Miles Dickinson <miles.dickinson92@gmail.com>
2014-12-22 09:10:59 -0500

    v0.0.2

--- script.js
+++ script.js
@@ -2,4 +2,22 @@
   
   $('nav.navbar').waypoint('sticky');
 
+  // Override Bootstrap default menu open onclick
+  $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a') ) {
+          $(this).collapse('hide');
+      }
+  });
+
+  // Navigation
+  $('.page').hide();
+  $('#home-page').show();
+
+  $('.welcome a, .navbar a').click(function(e) {
+    e.preventDefault();
+    var page = $(this).attr('href');
+    $('.page').hide();
+    $(page).fadeIn(500);
+  });
+
 }(jQuery));
